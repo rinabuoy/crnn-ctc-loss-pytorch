d1fee1f51332204b2987fddc01e1a08a0aa62ba7
dredwardhyde <komesergey@gmail.com>
2020-11-28 16:48:54 +0300

    Initial commit

---
+++ ctc_loss_example.py
@@ -0,0 +1,56 @@
+# this directly follows Graves et al's paper, in contrast to the production implementation, it does not use log-space
+import torch
+import torch.nn.functional
+import torch.backends.cudnn
+gpu = torch.device('cuda')
+
+
+def ctcloss_reference(log_probs, targets, input_lengths, target_lengths, blank=0, reduction='mean'):
+    input_lengths = torch.as_tensor(input_lengths, dtype=torch.long)
+    target_lengths = torch.as_tensor(target_lengths, dtype=torch.long)
+    dt = log_probs.dtype
+    log_probs = log_probs.double()  # we need the accuracy as we are not in logspace
+    targets = targets.long()
+    cum_target_lengths = target_lengths.cumsum(0)
+    losses = []
+    for i in range(log_probs.size(1)):
+        input_length = input_lengths[i].item()
+        target_length = target_lengths[i].item()
+        cum_target_length = cum_target_lengths[i].item()
+        targets_prime = targets.new_full((2 * target_length + 1,), blank)
+        if targets.dim() == 2:
+            targets_prime[1::2] = targets[i, :target_length]
+        else:
+            targets_prime[1::2] = targets[cum_target_length - target_length:cum_target_length]
+        probs = log_probs[:input_length, i].exp()
+        alpha = log_probs.new_zeros((target_length * 2 + 1,))
+        alpha[0] = probs[0, blank]
+        alpha[1] = probs[0, targets_prime[1]]
+        mask_third = (targets_prime[:-2] != targets_prime[2:])
+        for t in range(1, input_length):
+            alpha_next = alpha.clone()
+            alpha_next[1:] += alpha[:-1]
+            alpha_next[2:] += torch.where(mask_third, alpha[:-2], alpha.new_zeros(1))
+            alpha = probs[t, targets_prime] * alpha_next
+        losses.append(-alpha[-2:].sum().log()[None])
+    output = torch.cat(losses, 0)
+    if reduction == 'mean':
+        return (output / target_lengths.to(dtype=output.dtype, device=output.device)).mean()
+    elif reduction == 'sum':
+        return output.sum()
+    output = output.to(dt)
+    return output
+
+
+target_lengths = [30, 25, 20]
+input_lengths = [50, 50, 50]
+targets = torch.randint(1, 15, (sum(target_lengths),), dtype=torch.int)
+log_probs = torch.randn(50, 3, 15, dtype=torch.float, device=gpu).log_softmax(2)
+res = torch.nn.functional.ctc_loss(log_probs, targets, input_lengths, target_lengths)
+expected = ctcloss_reference(log_probs, targets.cuda(), input_lengths, target_lengths).float()
+with torch.backends.cudnn.flags(enabled=False):
+    res2 = torch.nn.functional.ctc_loss(log_probs, targets.cuda().long(), input_lengths, target_lengths)
+# tensor(4.0009, device='cuda:0') tensor(4.0009, device='cuda:0')
+# tensor(4.0009, device='cuda:0') tensor(4.0009, device='cuda:0')
+print(res, expected)
+print(res2, res)

ffccbcdbb04cfedfe8dcd098ae44e3b04eb645dc
edwardhyde <komesergey@gmail.com>
2020-11-30 18:06:13 +0300

    Добавил комментарии

--- ctc_loss_example.py
+++ ctc_loss_example.py
@@ -17,21 +17,40 @@ def ctcloss_reference(log_probs, targets, input_lengths, target_lengths, blank=0
         input_length = input_lengths[i].item()
         target_length = target_lengths[i].item()
         cum_target_length = cum_target_lengths[i].item()
+        # ==========================================================================================================
+        # To allow for blanks in the output paths, we consider a modified label sequence l', with blanks added to the
+        # beginning and the end and inserted between every pair of labels. The length of l' is therefore 2|l| + 1.
         targets_prime = targets.new_full((2 * target_length + 1,), blank)
         if targets.dim() == 2:
             targets_prime[1::2] = targets[i, :target_length]
         else:
             targets_prime[1::2] = targets[cum_target_length - target_length:cum_target_length]
+        # ==========================================================================================================
+        # переход из log space
         probs = log_probs[:input_length, i].exp()
+        # ==========================================================================================================
         alpha = log_probs.new_zeros((target_length * 2 + 1,))
+        # инициализируем начало sequence'а вероятностью blank'а как в paper в формуле 6
         alpha[0] = probs[0, blank]
+        # второй элемент инициализируем опять же как в формуле 6
         alpha[1] = probs[0, targets_prime[1]]
+        # эта маска валидна только когда целевой таргет либо blank либо равен таргету на 2 шага назад
         mask_third = (targets_prime[:-2] != targets_prime[2:])
+        # проходим по всей длине последовательности
         for t in range(1, input_length):
+            # тут мы сначала клонируем текущую альфу
             alpha_next = alpha.clone()
+            # потом сдвигаем альфу на один элемент вправо и складываем фактически At + (At-1)
             alpha_next[1:] += alpha[:-1]
+            # и только для тех элементов, где маска проходит, мы прибавляем еще и At-2
             alpha_next[2:] += torch.where(mask_third, alpha[:-2], alpha.new_zeros(1))
+            # так как мы идем слева направо, и альфа инициализирована нулями, то это тоже фактически маска
+            # и теперь мы перемножаем вероятности на каждом timestep с этой маской
+            # к последнему элементу последовательности здесь будет находиться произведение вероятности этого элемента
+            # на всю историю которая была левее него, т.е. фактически
+            # это расчет стоимости пути в графе с помощью динамического программирования
             alpha = probs[t, targets_prime] * alpha_next
+        # ==========================================================================================================
         losses.append(-alpha[-2:].sum().log()[None])
     output = torch.cat(losses, 0)
     if reduction == 'mean':

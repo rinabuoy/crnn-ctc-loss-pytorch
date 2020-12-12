2ac30b4ba9b75d97caaaadfcb5097d71effefb67
komesergey <komesergey@gmail.com>
2020-12-12 18:44:29 +0300

    Refactoring

--- mnist_sequence_recognition.py
+++ mnist_sequence_recognition.py
@@ -60,18 +60,18 @@ val_loader = torch.utils.data.DataLoader(val_set, batch_size=1, shuffle=True)
 
 
 # ================================================= MODEL ==============================================================
-class CnnGru(nn.Module):
+class CRNN(nn.Module):
 
     def __init__(self):
-        super(CnnGru, self).__init__()
+        super(CRNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3))
-        self.batch_norm1 = nn.InstanceNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=2)
-        self.batch_norm3 = nn.InstanceNorm2d(32)
-        self.conv5 = nn.Conv2d(32, 64, kernel_size=(3, 3))
-        self.batch_norm5 = nn.InstanceNorm2d(64)
-        self.conv6 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=2)
-        self.batch_norm6 = nn.InstanceNorm2d(64)
+        self.norm1 = nn.InstanceNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=2)
+        self.norm2 = nn.InstanceNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3))
+        self.norm3 = nn.InstanceNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=2)
+        self.norm4 = nn.InstanceNorm2d(64)
         self.gru_input_size = cnn_output_height * 64
         self.gru = nn.GRU(self.gru_input_size, gru_hidden_size, gru_num_layers, batch_first=True, bidirectional=True)
         self.fc = nn.Linear(gru_hidden_size * 2, num_classes)
@@ -80,16 +80,16 @@ class CnnGru(nn.Module):
         batch_size = x.shape[0]
         out = self.conv1(x)
         out = F.leaky_relu(out)
-        out = self.batch_norm1(out)
+        out = self.norm1(out)
+        out = self.conv2(out)
+        out = F.leaky_relu(out)
+        out = self.norm2(out)
         out = self.conv3(out)
         out = F.leaky_relu(out)
-        out = self.batch_norm3(out)
-        out = self.conv5(out)
+        out = self.norm3(out)
+        out = self.conv4(out)
         out = F.leaky_relu(out)
-        out = self.batch_norm5(out)
-        out = self.conv6(out)
-        out = F.leaky_relu(out)
-        out = self.batch_norm6(out)
+        out = self.norm4(out)
         out = out.permute(0, 3, 2, 1)
         out = out.reshape(batch_size, -1, self.gru_input_size)
         out, _ = self.gru(out)
@@ -97,7 +97,7 @@ class CnnGru(nn.Module):
         return out
 
 
-model = CnnGru().to(gpu)
+model = CRNN().to(gpu)
 criterion = nn.CTCLoss(blank=blank_label, reduction='mean', zero_infinity=True)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
